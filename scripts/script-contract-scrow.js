b47d9e6caa3527aeff0747f578bac20e6438fc10
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2020-11-27 20:16:20 -0400

    Update script-contract-scrow.js

--- scripts/script-contract-scrow.js
+++ scripts/script-contract-scrow.js
@@ -1,61 +1,68 @@
+//cria chave e um cliente para o médico
+// client para o médico realizar as transações
 const doctorSeed = loadOrCreateMnemonic("doctor.key");
 const {address: doctorAddr, client: doctorClient} = await connect(doctorSeed, {});
 
-// patient doesn't have a client here as we will not
-// submit any tx with this account, just query balance
+
+// cria chave para o paciente
+// cria um endereço para o paciente
 const patientSeed = loadOrCreateMnemonic("patient.key");
 const patientAddr = await mnemonicToAddress(patientSeed);
+
 
 console.log(doctorAddr, patientAddr);
 
 
+
+// cria uma conta para o médico
 doctorClient.getAccount();
-// if "undefined", do the following
 hitFaucet(defaultFaucetUrl, doctorAddr, "SHELL")
 doctorClient.getAccount();
 
 
-// check patientAddr has no funds
+// verifica se o paciente possui tokens
 doctorClient.getAccount(patientAddr);
 
-// get the working directory (needed later)
+// diretório atual
 process.cwd()
 
 
 
+// leitura do arquivo contrato
 const wasm = fs.readFileSync('contract.wasm');
-// you can add extra information to contract details such as source and builder.
-const up = await doctorClient.upload(wasm, { source: "https://crates.io/api/v1/crates/cw-escrow/0.6.0/download", builder: "cosmwasm/rust-optimizer:0.9.0"});
+
+// deploy do contrato na rede 
+const up = await doctorClient.upload(wasm, { source: "medical-application", builder: "cosmwasm/rust-optimizer:0.9.0"});
 
 console.log(up);
 const { codeId } = up;
 
-const initMsg = {arbiter: doctorAddr, recipient: patientAddr};
-const { contractAddress } = await doctorClient.instantiate(codeId, initMsg, "medical-contract", { memo: "memo",transferAmount: [{denom: "ushell", amount: "50000"}]});
 
-// check the contract is set up properly
+// estado inicial do contrato com o endereço do médico e do paciente.
+const initMsg = {arbiter: doctorAddr, recipient: patientAddr};
+const { contractAddress } = await doctorClient.instantiate(codeId, initMsg, "medical-contract-test", { memo: "memo",transferAmount: [{denom: "ushell", amount: "50000"}]});
+
+// verifica o endereço do contrato
 console.log(contractAddress);
 doctorClient.getContract(contractAddress);
 doctorClient.getAccount(contractAddress);
 
-// make a raw query - key length prefixed "config"
-const key = new Uint8Array([0, 6, ...toAscii("config")]);
-const raw = await doctorClient.queryContractRaw(contractAddress, key);
-JSON.parse(fromUtf8(raw))
-// note the addresses are stored in base64 internally, not bech32, but the data is there... this is why we often implement smart queries on real contracts
+
+
+
+// cria uma prescrição
+const approve = {approve: {quantity: [{
+	amount: "1", denom: "ushell",
+	medicine:"recommended medicine",
+	diagnostic:"diagnostic description"}]}};
 
 
 
 
-const approve = {approve: {quantity: [{amount: "1", denom: "ushell", medicine:"recommended drug",diagnostic:"diagnostic description"}]}};
 
-
-
-// but doctor can (and moves tokens to patient)
+//prescrição é enviada para a rede para ser validada
 doctorClient.execute(contractAddress, approve);
-// verify patient got the tokens
-doctorClient.getAccount(patientAddr);
-// verify contract lost
-doctorClient.getAccount(contractAddress);
 
+// verifica a quantidade de prescrições já realizadas e aprovadas do paciente.
+doctorClient.getAccount(patientAddr);
 
